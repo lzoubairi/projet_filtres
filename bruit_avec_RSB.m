03d4d23c5ef27c2d25a45d065e8b25f67effbe5c
ZOUBAIRI Laila <Laila.Zoubairi@bordeaux-inp.fr>
2019-11-26 16:29:25 +0100

    preliminaire 2 done

--- bruit_avec_RSB.m
+++ bruit_avec_RSB.m
@@ -1,20 +1,27 @@
 function sigf = bruit_avec_RSB(sig0, RSB) %RSB en dB
 
-l = length(sig0);
-N = 500;
-% m = 0;
-% var = 1;
+    l = length(sig0);
+    bruit=randn(1, l);
+    % m = 0;
+    % var = 1;
+    %bruit = m + var*randn(1,N);
 
-
-%bruit = m + var*randn(1,N);
-
-P0=norm(sig0,2)^2/N; %puissance du signal original
-
-ec=P0/(10^(RSB/10));
-
-bruit=0.2*sqrt(ec)*randn(N,1);
-
-sigf = sig0 + bruit;
+    % Calcul des puissances:
+    
+    P0=0;
+    Pb = 0;
+    for i=1:l
+        P0=P0+(sig0(i))^2;
+        Pb=Pb+(bruit(i))^2;
+    end
+    P0=P0/l; %pas en dB
+    Pb=Pb/l;
+    
+    % Ajout du bruit:
+    
+    var = (P0/Pb)*10^(-RSB/10);
+    bruitf = sqrt(var)*bruit;
+    sigf = sig0 + bruitf;
 
 end
 
--- proj1.m
+++ proj1.m
@@ -8,52 +8,65 @@ clc
 
 s = load("fcno04fz.mat");
 signal = s.fcno04fz; %57344x1
+signal = signal';
+% figure;
+% plot(signal);
+% title('signal de parole');
 
 N = 500;
 m = 0;
+l = length(signal);
 sigma = 1;
 var = sigma^2;
-axet = 0:10/(N-1):10;
+axet = 0:10/(l-1):10;
 axef = -1/2:1/N:1/2-1/N;
 
 
 %Bruit blanc gaussien
 
 BBG = m + var*randn(1,N);
-figure
-plot(axet, BBG);
-title('BBG');
+% figure
+% plot(axet, BBG);
+% title('BBG');
 
 
 %fonction autocorr théorique:
 
 Rss_th = 1/N * xcorr(BBG);
-figure
-plot(Rss_th);
-title('autocorr théorique');
+% figure
+% plot(Rss_th);
+% title('autocorr théorique');
 
 
 %spectre de puissance:
 
 Sss = 1/N * abs(fftshift(fft(BBG))).^2;
-figure
-plot(axef, Sss);
-title('spectre de puissance théorique');
+% figure
+% plot(axef, Sss);
+% title('spectre de puissance théorique');
 
 
 %fonction autocorr estimée:
 
-[C_biased,~] =xcorr(BBG,'biased'); %estimateur biaisé
-figure
-plot(C_biased);
-title('Rss avec estimateur biaisé');
-[C_unbiased,~] =xcorr(BBG,'unbiased'); %estimateur non biaisé
-figure
-plot(C_unbiased);
-title('Rss avec estimateur non biaisé');
+% [C_biased,~] =xcorr(BBG,'biased'); %estimateur biaisé
+% figure
+% plot(C_biased);
+% title('Rss avec estimateur biaisé');
+% [C_unbiased,~] =xcorr(BBG,'unbiased'); %estimateur non biaisé
+% figure
+% plot(C_unbiased);
+% title('Rss avec estimateur non biaisé');
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%Fonction MATLAB Pwelch:
+
+
+% figure;
+% plot(pwelch(BBG));
+% title('pwelch');
 
 
 %Periodogramme de Daniell:
@@ -71,12 +84,12 @@ for i=pas+1:l-pas
     tab_moy(i) = sum/(2*pas + 1);
 end
 
-figure;
-plot(tab_moy);
-title('Daniell');
+% figure;
+% plot(axef, tab_moy);
+% title('Daniell spectrogram');
 
 
-% Periodogramme de Welch:
+% Periodogramme de Welch (marche pas):
 
 nsegment = 50; %nombre de décompositions du signal
 x = BBG;
@@ -90,10 +103,46 @@ end
 avgDSP = sumdsp/ nsegment;
 
 % figure
-% plot(avgDSP);
+% plot(axef, avgDSP);
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%% Préliminaire 2 :
+
+RSB1 = 5;
+RSB2 = 10;
+RSB3 = 15;
+
+sigf1 = bruit_avec_RSB(signal, RSB1);
+sigf2 = bruit_avec_RSB(signal, RSB2);
+sigf3 = bruit_avec_RSB(signal, RSB3);
+
+%Affichage + spectrogramme du signal original:
+
+figure,
+
+subplot(2,1,1);
+plot(axet,signal);
+title('Représentation temporelle de s');
+
+subplot(2,1,2);
+spectrogram(signal);
+title('Spectrogramme de s');
+
+%Affichage signal bruité + spectrogramme:
+
+figure,
+
+subplot(2,1,1);
+plot(axet,sigf1);
+title('Représentation temporelle de s bruité');
+
+subplot(2,1,2);
+spectrogram(sigf1);
+title('Spectrogramme de s bruité');
+
 
 
 %Corrélogramme:
